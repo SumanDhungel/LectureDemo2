a8f806e3f8bc1e268012393b375458cd69be6922
SumanDhungel <suman.dhungel@metropolia.fi>
2024-08-22 11:38:21 +0300

    Second commit integrated demo with variables

--- Variabledemo.py
+++ Variabledemo.py
@@ -1,3 +1,24 @@
+
+'''
+num1 = int(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+sum  = num1 + num2
+print(sum)
+'''
+name = input("Enter your name: ")
+school = input("Enter your school: ")
+floatVariable = 12.3456
+print("You are:", name,  "and you study at: ", school)
+print(f" Your name is {name}, and your float Variable is, {floatVariable}")
+
+rds = float(input("Enter RDS: "))
+area = 3.14 * (rds ** 2)
+print (f"The RDS is, {rds}, and your area is, {area: .2f}")
+
+
+
+
+'''
 intVariable = 4
 floatVariable = 4.5
 stringVariable = "Software 1 is fun"
@@ -9,3 +30,14 @@ print(stringVariable)
 print(type(intVariable))
 print(type(floatVariable))
 print(type(stringVariable))
+
+#typecasting is fun
+intVariable = int(floatVariable)
+print("here is the int version of note variable", intVariable)
+
+shareOfLoan = 500.00/3
+print(shareOfLoan)
+print(int(shareOfLoan))
+print(type(shareOfLoan))
+'''
+
